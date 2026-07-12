4e2c25c1cadb4ee30c8a0f5ad89000d69575c6d1
Jennifer Schieffer <jschieffer498@outlook.com>
2026-07-12 12:33:12 -0500

    initial commit

---
+++ Printing.py
@@ -0,0 +1,81 @@
+#Printing to the console
+print("Hello world") # prints statement in ""
+
+"Hello world" 
+
+#Newline Character
+# prints to separate lines
+print("Hello")
+print("world")
+
+# prints to same line
+print("Hello ", end='')
+print("world")
+
+# prints to same line
+print("Hello ", end="")
+print("world")
+
+# prints with a tab character
+print("Hello ", end='\t')
+print("world")
+
+# prints with a custom ending character, !, after first word
+print("Hello ", end='!')
+print("world")
+
+# prints with a custom ending character, !
+print("Hello ", end='')
+print("world!")
+
+# Comments
+
+#This is a Python comment
+print("This is regular Python code")
+
+'''
+This is a multi-line comment
+Try making one at the end of the code file
+The IDE (this window) tries to help by adding the ending triple quotes automatically
+'''
+
+#add code below this line
+#. . . .   
+print("Red")
+print("Orange") #the comment STARTS at the hash symbol
+#print("Yellow");
+print("Green")
+print("Blue")
+print("Indigo")
+print("Violet")
+print("These are the colors of a rainbow!");
+#. . . .    
+#add code above this line
+
+# Comment out line of code with error
+#add code below this line
+#. . . .   
+# prnt("Red")
+print("Orange") 
+print("Yellow")
+print("Green")
+print("Blue")
+print("Indigo")
+print("Violet")
+print("These are the colors of a rainbow!");
+#. . . .    
+#add code above this line
+
+#Comment blocks
+
+'''
+This is a multi-line comment
+You can then easily end the comment with a triple quote (see below)
+'''
+
+#You can also do a multi-line comment
+#like this!
+    
+print("Notice code that runs is not the same color as single-line comments");
+print("This feature is called syntax highlighting");
+print("It is a common feature of IDEs");
